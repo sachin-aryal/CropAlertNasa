94dc4532d2d6efcfd5fe62c539b40491aa892b97
sachin-aryal <sachin91719@gmail.com>
2015-04-11 11:56:57 +0545

    Commit 1

--- grails-app/domain/nasacropalert/Crop.groovy
+++ grails-app/domain/nasacropalert/Crop.groovy
@@ -6,9 +6,11 @@ class Crop {
     String pesticide
     String location
     String affected
+    String improve
     Date startDate
     byte[] image
     String description
+    static belongsTo = ['user':User]
     static constraints = {
         image(maxSize: 904800)
     }
