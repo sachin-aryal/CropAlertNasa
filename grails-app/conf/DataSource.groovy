35037bf35d52fcfd31cc504dff44d288cda433f9
girisagar46 <girisagar46@gmail.com>
2015-04-11 16:21:26 +0545

    Sagar command line ViewData.jsp commit push

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -3,7 +3,7 @@ dataSource {
     jmxExport = true
     driverClassName = "com.mysql.jdbc.Driver"
     username = "root"
-    password = ""
+    password = "iam@Dwit!23AD1"
 }
 hibernate {
     cache.use_second_level_cache = true
---
+++ test/unit/nasacropalert/CropControllerSpec.groovy
@@ -0,0 +1,20 @@
+package nasacropalert
+
+import grails.test.mixin.TestFor
+import spock.lang.Specification
+
+/**
+ * See the API for {@link grails.test.mixin.web.ControllerUnitTestMixin} for usage instructions
+ */
+@TestFor(CropController)
+class CropControllerSpec extends Specification {
+
+    def setup() {
+    }
+
+    def cleanup() {
+    }
+
+    void "test something"() {
+    }
+}
